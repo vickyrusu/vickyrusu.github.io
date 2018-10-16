9517539d7a3af5399012e337afa0606f8a50317a
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-10-16 22:00:14 +0300

    Update html5/ec2_1.js

--- html5/ec2_1.js
+++ html5/ec2_1.js
@@ -15,11 +15,11 @@ function citire()
 
 function rezolvare(coeficienti)
 {
-  var delta = coeficienti.b*coeficienti.b-4*coeficienti.a*coeficieti.c;
+     var delta = coeficienti.b*coeficienti.b-4*coeficienti.a*coeficieti.c;
      var x1_re, x1_im, x2_re, x2_im;
      if (delta >=0){
      	x1_re = (-coeficienti.b + Math.sqrt(delta)) / 2 * coeficienti.a;
-        x2_im = (-coeficienti.b - Math.sqrt(delta)) / 2 * coeficienti.a;
+        x2_re = (-coeficienti.b - Math.sqrt(delta)) / 2 * coeficienti.a;
         x1_im = x2_im = 0;
      }
      else{
@@ -27,20 +27,18 @@ function rezolvare(coeficienti)
           x1_im = Math.sqrt(-delta) / (2*coeficienti.a);
           x2_re = -coeficienti.b/(2*coeficienti.a);
           x2_im = -Math.sqrt(-delta) / (2*coeficienti.a);
-
-
      }
      var x1 = {re: x1_re, im: x1_im};
-      var x2 = {re: x2_re, im: x2_im};
-      var solutii = {x1:x1, x2:x2};
-   return solutii;
+     var x2 = {re: x2_re, im: x2_im};
+     var solutii = {x1:x1, x2:x2};
+     return solutii;
 }
 
 
 function afisare()
 {
  document.getElementById("id_x1").innerHTML = solutii.x1_re + " +" + solutii.x1_im + "i";
-   document.getElementById("id_x2").innerHTML = solutii.x2_re + " +" + solutii.x2_im + "i";
+ document.getElementById("id_x2").innerHTML = solutii.x2_re + " +" + solutii.x2_im + "i";
 }
 
 
