268813af50e7c9a44e384e1e556eaa00ef73ca18
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-10-22 12:40:58 +0300

    rotire_cerc

---
+++ html5/desenare.js
@@ -0,0 +1,39 @@
+document.getElementById("id_bussiness_version").innerHTML = "Business version: 2018.10.15.5";
+document.getElementById("id_start_button").addEventListener("click", start);
+document.getElementById("id_stop_button").addEventListener("click", stop);
+
+document.getElementById("id_stop_button").disabled = true;
+document.getElementById("id_start_button").disabled = false;
+
+var unghi_start = { unghi: 0 }; //in grade
+
+function deseneaza_cerc(unghi, context, w, h) {
+    //clearRect este o metoda cu care stergem dreptunghiul
+    //sau desenam de fapt un dreptunghi alb peste
+    context.clearRect(0, 0, w, h);
+
+    context.beginPath();
+    context.arc(w / 2 + 100 * Math.cos(unghi.unghi * Math.PI / 180),
+        h / 2 + 100 * Math.sin(unghi.unghi * Math.PI / 180),
+        50, 0, 2 * Math.PI);
+    context.stroke();
+    unghi.unghi++;
+}
+
+
+function start() {
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+
+    document.getElementById("id_start_button").disabled = true;
+    document.getElementById("id_stop_button").disabled = false;
+
+    id_timer = setInterval(deseneaza_cerc, 10, unghi_start, context, canvas.width, canvas.height);
+
+}
+
+function stop() {
+    document.getElementById("id_start_button").disabled = false;
+    document.getElementById("id_stop_button").disabled = true;
+    clearInterval(id_timer);
+}
