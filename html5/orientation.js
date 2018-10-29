927bdca39705c0aa51950c2b5b5e7849c50f3360
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-10-29 12:50:38 +0200

    update

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.0";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.29.1";
 
 window.addEventListener("deviceorientation", on_device_orientation);
 
@@ -6,4 +6,14 @@ function on_device_orientation(e) {
     document.getElementById("id_alpha").innerHTML = e.alpha;
     document.getElementById("id_beta").innerHTML = e.beta;
     document.getElementById("id_gamma").innerHTML = e.gamma;
+
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    var R = 10;
+    constext.beginPath();
+    constext.arc(canvas.width / 2 + e.gamma / (90 * canvas.width / 2 - R), canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R), 10, 0, 2 * Math.PI);
+    context.stroke();
 }
