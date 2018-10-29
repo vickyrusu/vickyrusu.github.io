24934cf7891db0f20a83c0e46c0801dffb7c3f9d
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-10-29 10:33:56 +0200

    lucru cu worker

--- html5/desenare.js
+++ html5/desenare.js
@@ -28,6 +28,11 @@ function start() {
     document.getElementById("id_start_button").disabled = true;
     document.getElementById("id_stop_button").disabled = false;
 
+    var my_worker = new Worker("calcul_prime.js");
+    my_worker.onmessage = function(e) {
+        document.getElementById("id_prime").innerHTML = e.data;
+    }
+
     id_timer = setInterval(deseneaza_cerc, 10, unghi_start, context, canvas.width, canvas.height);
 
 }
