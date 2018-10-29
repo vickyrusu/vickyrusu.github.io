989b9f18cfa536eb4ed4fca4aedb572dc9b27cb2
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-10-29 12:00:12 +0200

    upd

--- html5/calcul_prime.js
+++ html5/calcul_prime.js
@@ -1,39 +1,35 @@
 var stopped = false;
-//----------
-
-
+//-------------------------------------------
 onmessage = function(e) {
         if (e.data == "stop") {
             stopped = true;
+        } else
+        if (e.data == "start") {
+            stopped = false;
         }
     }
-    //------verificam nr prim -----------
+    //-------------------------------------------
 function is_prim(n) {
     for (var i = 2; i * i <= n; i++)
         if (n % i == 0)
             return false;
     return true;
 }
-
-//-------------------
+//-------------------------------------------
 function calculeaza_prime(start_value) {
-
     if (stopped)
     ;
     else {
         for (var i = start_value; i < start_value + 1000; i++) {
-
             if (is_prim(i))
                 postMessage(i);
-
         }
         start_value += 1000;
     }
     if (start_value < 1e10)
-        setTimeout(calculeaza_prime, 1, start_value); //sta 1 milisec pana calculeaza prime
+        setTimeout(calculeaza_prime, 1, start_value);
     else
         postMessage("gata");
 }
-
-//-------------
+//-------------------------------------------
 calculeaza_prime(1e9);
