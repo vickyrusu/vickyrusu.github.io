5928f83e387694ba1eeb44cab092443ac8025e05
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-10-29 13:08:25 +0200

    desenare cerc

--- html5/orientation.js
+++ html5/orientation.js
@@ -2,6 +2,7 @@ document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 
 
 window.addEventListener("deviceorientation", on_device_orientation);
 
+//------------------------------------------------------
 function on_device_orientation(e) {
     document.getElementById("id_alpha").innerHTML = e.alpha;
     document.getElementById("id_beta").innerHTML = e.beta;
@@ -13,7 +14,11 @@ function on_device_orientation(e) {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     var R = 10;
-    constext.beginPath();
-    constext.arc(canvas.width / 2 + e.gamma / (90 * canvas.width / 2 - R), canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R), 10, 0, 2 * Math.PI);
+    context.beginPath();
+    context.arc(canvas.width / 2 + e.gamma / 90 * (canvas.width / 2 - R),
+        canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R),
+        R,
+        0, 2 * Math.PI);
     context.stroke();
 }
+//------------------------------------------------------
