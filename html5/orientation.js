917b64f2d17f188a91e0261f854c2df23816da3b
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-11-05 10:49:08 +0200

    updates

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.05.1";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.05.3";
 
 window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
@@ -24,7 +24,15 @@ function on_device_orientation(e) {
 }
 //------------------------------------------------------
 function on_device_motion(e) {
-    document.getElementById("id_acc_z").innerHTML = Math.round(e.accelerationIncludingGravity.z * 100) / 100;
-    document.getElementById("id_acc_x").innerHTML = Math.round(e.accelerationIncludingGravity.x * 100) / 100;
-    document.getElementById("id_acc_y").innerHTML = Math.round(e.accelerationIncludingGravity.y * 100) / 100;
+    var acc_z = e.accelerationIncludingGravity.z;
+    var acc_x = e.accelerationIncludingGravity.x;
+    var acc_y = e.accelerationIncludingGravity.y;
+
+    document.getElementById("id_acc_z").innerHTML = Math.round(acc_z * 100) / 100;
+    document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100;
+    document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
+
+    document.getElementById("id_rot_x").innerHTML = Math.atan(acc_x / acc_z) * 180 / Math.PI;
+    document.getElementById("id_rot_y").innerHTML = Math.atan(acc_y / acc_z) * 180 / Math.PI;
+
 }
