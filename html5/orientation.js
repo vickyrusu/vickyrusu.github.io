3471cd487e59468e11ad701e9ac935ec6d378a6c
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-11-05 10:55:28 +0200

    cu minus

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.05.3";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.05.4";
 
 window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
@@ -32,7 +32,7 @@ function on_device_motion(e) {
     document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100;
     document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
 
-    document.getElementById("id_rot_x").innerHTML = Math.atan(acc_x / acc_z) * 180 / Math.PI;
+    document.getElementById("id_rot_x").innerHTML = -Math.atan(acc_x / acc_z) * 180 / Math.PI;
     document.getElementById("id_rot_y").innerHTML = Math.atan(acc_y / acc_z) * 180 / Math.PI;
 
 }
