b37bf02abc687b5547f3cb32ba0a31f24c642df1
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-11-12 11:36:02 +0200

    mouse touch

--- html5/cam.js
+++ html5/cam.js
@@ -1,6 +1,7 @@
-document.getElementById("id_bussiness_version").innerHTML = "Business version: 2018.11.12.1";
+document.getElementById("id_bussiness_version").innerHTML = "Business version: 2018.11.12.2";
 document.getElementById("id_button").addEventListener("click", start);
-
+document.getElementById("id_video").addEventListener("touchstart", on_touch);
+document.getElementById("id_video").addEventListener("mousedown", on_touch);
 
 function on_success(e) {
 
@@ -13,8 +14,14 @@ function on_failure(e) {
 }
 //---------------
 function start() {
-    var c = { audio: true, video: { facingMode: "environment" } };
-    //accesare camera fata
+    var c = { audio: true, video: { facingMode: "user" } };
+    //camera spate o accesam cu facingMode: "environment"
     navigator.mediaDevices.getUserMedia(c).then(on_success).catch(on_failure);
 }
 //----------------
+function on_touch(e) {
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+    var video = document.getElementById("id_video");
+    context.drawImage(video, 0, 0, );
+}
