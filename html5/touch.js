b3839a492765dd6ab135afb0261d9c0158c65058
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-11-26 11:31:30 +0200

    prevent default

--- html5/touch.js
+++ html5/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.2";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.3";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
@@ -20,6 +20,9 @@ function on_touch_start(e) {
 }
 //-------------------------
 function on_touch_move(e) {
+
+    e.preventDefault();
+
     for (var i = 0; i < e.changedTouches.length; i++) {
         var context = canvas.getContext("2d");
         context.beginPath();
