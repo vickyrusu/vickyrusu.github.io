239eee117884c7b3e858b3c3a894508c16e1d3e7
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-12-03 12:47:14 +0200

    Update svg.js

--- html5/svg.js
+++ html5/svg.js
@@ -1,18 +1,19 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.03.0";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.03.2";
 
 window.addEventListener("deviceorientation", on_device_orientation);
 //window.addEventListener("devicemotion", on_device_motion);
 
-//------------------------------------------------------
+//---------------------------------------------------------------------------
 function on_device_orientation(e) {
-
-    var R = 20;
-    var svg_width = svg.getAttribute("width");
-    var svg_height = svg.getAttribute("height");
     var svg = document.getElementById("id_svg");
     var circle = document.getElementById("id_circle");
-    circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R) + "px");
-    circle.setAttribute("cy", svg.height / 2 + e.beta / 90 * (svg.width / 2 - R) + "px");
 
+    var R = 20;
+
+    var svg_width = svg.getAttribute("width");
+    var svg_height = svg.getAttribute("height");
+
+    circle.setAttribute("cx", svg_width / 2 + e.gamma / 90 * (svg_width / 2 - R) + "px");
+    circle.setAttribute("cy", svg_height / 2 + e.beta / 90 * (svg_height / 2 - R) + "px");
 }
-//------------------------------------------------------
+//---------------------------------------------------------------------------
