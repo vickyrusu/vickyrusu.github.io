5d0cdc22126bb3ad608efd5880e81be28eec2121
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-12-03 12:23:46 +0200

    update js

---
+++ html5/svg.js
@@ -0,0 +1,15 @@
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.03.0";
+
+window.addEventListener("deviceorientation", on_device_orientation);
+window.addEventListener("devicemotion", on_device_motion);
+
+//------------------------------------------------------
+function on_device_orientation(e) {
+
+
+    var circle = document.getElementById("id_circle");
+    circle.setAttribute("cx", );
+    circle.setAttribute("cy", );
+
+}
+//------------------------------------------------------
