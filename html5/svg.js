e6bb0bef7287cae3297cbe24d2d0ad3b0682115d
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-12-03 12:37:12 +0200

    Update svg.js

--- html5/svg.js
+++ html5/svg.js
@@ -1,7 +1,7 @@
 document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.12.03.0";
 
 window.addEventListener("deviceorientation", on_device_orientation);
-window.addEventListener("devicemotion", on_device_motion);
+//window.addEventListener("devicemotion", on_device_motion);
 
 //------------------------------------------------------
 function on_device_orientation(e) {
@@ -9,8 +9,8 @@ function on_device_orientation(e) {
     var R = 20;
     var svg = document.getElementById("id_svg");
     var circle = document.getElementById("id_circle");
-    circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R));
-    circle.setAttribute("cy", svg.width / 2 + e.beta / 90 * (svg.width / 2 - R));
+    circle.setAttribute("cx", svg.width / 2 + e.gamma / 90 * (svg.width / 2 - R) + "px");
+    circle.setAttribute("cy", svg.width / 2 + e.beta / 90 * (svg.width / 2 - R) + "px");
 
 }
 //------------------------------------------------------
