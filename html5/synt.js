621c57f636161c32538a337ec31e57c9dee41633
vickyrusu <43953243+vickyrusu@users.noreply.github.com>
2018-12-10 11:46:24 +0200

    text to speach

---
+++ html5/synt.js
@@ -0,0 +1,21 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version 2018.12.10.0";
+document.getElementById("id_get_voices").addEventListener("click", get_voices);
+document.getElementById("id_start").addEventListener("click", speak);
+var synth = window.speechSynthesis;
+
+function get_voices() {
+    var voices = synth.getVoices();
+    var voices_list = document.getElementById("id_voices_list");
+    for (var i = 0; i < synth.getVoices().length; i++)
+        voices_list.innerHTML = voices[i].lang + " " + voices[i].name + "<br>";
+}
+
+//--------------------------------------
+function speak() {
+    var utterance = new SpeechSynthesisUtterance();
+    utterance.lang = " en-US";
+    utterance.text = document.getElementById("id_text").value;
+
+    synth.speak(utterance);
+}
+//----------------------------------
